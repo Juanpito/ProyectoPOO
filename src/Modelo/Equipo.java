f35e8c09e99a54948aa99afa7ce0183eb0087a71
Juan <juan.salas2201@alumnos.ubiobio.cl>
2022-10-18 19:48:59 -0300

    Modelo, cliente y Equipo hecho.

--- src/Modelo/Equipo.java
+++ src/Modelo/Equipo.java
@@ -1,5 +1,31 @@
 package Modelo;
 
 public class Equipo {
+    private long codigo;
+    private String descripcion;
+    private long precioArriendoDia;
+    private EstadoEquipo estado=EstadoEquipo.OPERATIVO;
 
+    public Equipo(long codigo, String descripcion, long precioArriendoDia) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precioArriendoDia = precioArriendoDia;
+    }
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public long getPrecioArriendoDia() {
+        return precioArriendoDia;
+    }
+
+
+    public EstadoEquipo getEstado() {
+        return estado;
+    }
 }
