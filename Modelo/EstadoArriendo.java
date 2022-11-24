393447e3fdb0679b86247e4810894d74ec9a452a
JesusVivanco <116122855+JesusVivanco@users.noreply.github.com>
2022-11-24 00:28:35 -0300

    mala carpeta de subida

--- Modelo/EstadoArriendo.java
+++
@@ -1,5 +0,0 @@
-package Modelo;
-
-public enum EstadoArriendo{
-    INICIADO, ENTREGADO, DEVUELTO
-}
--- Modelo/EstadoEquipo.java
+++
@@ -1,7 +0,0 @@
-package Modelo;
-
-public enum EstadoEquipo {
-    OPERATIVO,
-    EN_REPARACION,
-    DADO_DE_BAJA
-}
